8b6d8c67a03f6fd93de47c2c099eeccd6c8dd202
Gustavo Gutiérrez <squgus@gmail.com>
2017-12-01 00:09:33 -0600

    Snake now moves, eats and prints correctly

--- proyectoFinalSnake.c
+++ proyectoFinalSnake.c
@@ -1,5 +1,5 @@
 /*
- * proyectoFinalSnake.c
+ * proyectoFinalSnake->c
  *
  * Created: 11/27/2017 5:02:44 PM
  * Coauthor: Gustavo Gutiérrez Gómez
@@ -16,7 +16,6 @@
    .equ __lcd_D7=5
 #endasm
  
- 
 #include <mega328P.h>
 #include <display.h>
 #include <delay.h>
@@ -24,61 +23,38 @@
 #include <stdlib.h>
 #include "matriz.h"
 
+// Direction constants
+const signed char UP = 0;
+const signed char RIGHT = 1;
+const signed char DOWN = 2;
+const signed char LEFT = 3;
+// Max length constant
+const signed char MAX_LENGTH_SNAKE = 24;
 
-typedef struct node {
-    int position;
-    struct node* next;
-    struct node* prev;
-}node;
-
-signed char snakePos;
-signed char pelletPos;
-unsigned char snakeLen;
-unsigned char points;
+// Game variables
 unsigned char direction;
-unsigned int speed;
-unsigned char pelletCatched=1;
+signed char snakeX[MAX_LENGTH_SNAKE]; // snakeX[0] is head
+signed char snakeY[MAX_LENGTH_SNAKE]; // snakeY[0] is head
+unsigned char snakeLength;
+unsigned char matrix[8][8];
 
-node* head;
-node* tail;
+signed char pelletX;
+signed char pelletY;
 
+signed char points;
+signed int speed;
+signed char gameRunning;
 
+// Iterator variables
+int i;
+int j;
 
+// Matrix variables
+unsigned int mX;
+unsigned int mY;
+unsigned int m;
 
-unsigned int dots[64] = {
-    0x0880, 0x0780, 0x0680, 0x0580, 0x0480, 0x0380, 0x0280, 0x0180,
-    0x0840, 0x0740, 0x0640, 0x0540, 0x0440, 0x0340, 0x0240, 0x0140,
-    0x0820, 0x0720, 0x0620, 0x0520, 0x0420, 0x0320, 0x0220, 0x0120,
-    0x0810, 0x0710, 0x0610, 0x0510, 0x0410, 0x0310, 0x0210, 0x0110,
-    0x0808, 0x0708, 0x0608, 0x0508, 0x0408, 0x0308, 0x0208, 0x0108,
-    0x0804, 0x0704, 0x0604, 0x0504, 0x0404, 0x0304, 0x0204, 0x0104,
-    0x0802, 0x0702, 0x0602, 0x0502, 0x0402, 0x0302, 0x0202, 0x0102,
-    0x0801, 0x0701, 0x0601, 0x0501, 0x0401, 0x0301, 0x0201, 0x0101
-};
-
-void addpellet(node* last_node, int position)
-{
-    //declara la nueva cola
-    node* new_node = (struct node *)malloc(sizeof(struct node));
-    //declara el nuevo final de la lista
-    last_node->prev = new_node;
-    new_node->next = last_node;          
-    new_node->prev = NULL;
-    new_node->position = position;
-    tail = new_node;
-
-}
-
-
-void printsnake(node* head)
-{
-    node* cursor = head;
-    while(cursor != NULL)
-    {
-        cursor = cursor->prev;
-    }
-}
-
+// Functions
 void clearMax() {
     MandaMax7219(0x0800);
     MandaMax7219(0x0700);
@@ -90,91 +66,208 @@ void clearMax() {
     MandaMax7219(0x0100);
 }
 
-void setPellet(){
-    while((pelletPos = rand()%64) == snakePos);
+void endGame() {
+    EraseLCD();
+    StringLCD("PERDISTE!");
+    MoveCursor(0, 1);
+    StringLCD("Pica ");
+    CharLCD(94);
+    StringLCD(" para jugar");
+    while (PIND.0);
+    gameRunning = 0;
 }
 
-unsigned char doCatchPellet() {
-    if (snakePos == pelletPos) {
-        points++;
-        if (points > 15) {
-            speed = 100;
-        } else {
-            speed = 500 - (points * 25);
+void moveSnake() {
+    for (i = snakeLength - 1; i > 0; i--) {
+    //     // if ((direction == RIGHT) && (snakeX[0]-snakeLength == 7)) {
+    //     //     snakeX[0] = -1;
+    //     // } else if ((direction == LEFT) && (snakeX[0]+snakeLength == 0)) {
+    //     //     snakeX[0] = 8;
+    //     // } else {
+            snakeX[i] = snakeX[i-1];
+    //     // }
+
+    //     // if ((direction == UP) && (snakeY[0]+snakeLength == 0)) {
+    //     //     snakeY[0] = 8;
+    //     // } else if ((direction == DOWN) && (snakeY[0]-snakeLength == 7)) {
+    //     //     snakeY[0] = -1;
+    //     // } else {
+            snakeY[i] = snakeY[i-1];
+    //     // }
+    }
+    switch (direction) {
+        case UP: {
+            snakeY[0] = snakeY[0] + 1;
+            if (snakeY[0] > 7) {
+                snakeY[0] = 0;
+            }
+            break;
         }
-        return 1;
+        case RIGHT: {
+            snakeX[0] = snakeX[0] - 1;
+            if (snakeX[0] < 0) {
+                snakeX[0] = 7;
+            }
+            break;
+        }
+        case DOWN: {
+            snakeY[0] = snakeY[0] - 1;
+            if (snakeY[0] < 0) {
+                snakeY[0] = 7; 
+            }
+            break;
+        }
+        case LEFT: {
+            snakeX[0] = snakeX[0] + 1;
+            if (snakeX[0] > 7) {
+                snakeX[0] = 0;
+            }
+            break;
+        }
+    }
+}
+
+void printBoard() {
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    clearMax();
+    for (i = 0; i < snakeLength; i++) {
+        matrix[snakeX[i]][snakeY[i]] = 1;
+    }
+    matrix[pelletX][pelletY] = 1;
+
+    mX = 256;
+    for (i = 0; i < 8; i++) {
+        mY = 0;
+        if (matrix[i][7]) {
+            mY++;
+        }
+        for (j = 6; j >= 0; j--) {
+            mY = mY << 1;
+            if (matrix[i][j]) {
+                mY++;
+            }
+        }
+        m = mX + mY;
+        MandaMax7219(m);
+        mX = mX + 256;
+    }
+}
+
+// void growSnake() {
+//     //declara el nuevo final de la lista
+//     tail->prev = new_node;
+//     new_node->next = tail;
+//     new_node->prev = NULL;
+//     new_node->position = lastPos;
+//     tail = new_node;
+// }
+
+char isCollinding(signed char x, signed char y) {
+    for (i = 0; i < snakeLength - 1; i++) {
+        if ((x == snakeX[i]) && (y == snakeY[i])) {
+            return 1;
+        }
     }
     return 0;
 }
 
+void setPellet(){
+    do {
+        pelletX = rand()%8;
+        pelletY = rand()%8;
+    } while (isCollinding(pelletX, pelletY));
+}
+
+void increaseSpeed() {
+    if (points > 15) {
+        speed = 100;
+    } else {
+        speed = 500 - (points * 25);
+    }
+}
+
+void doEatPellet() {
+    if ((snakeX[0] == pelletX) && (snakeY[0] == pelletY)) {
+        snakeLength++;
+        points++;
+        setPellet();
+        increaseSpeed();
+    }
+}
+
+void doEatSnake() {
+    for (i = 1; i < snakeLength; i++) {
+        if ((snakeX[0] == snakeX[i]) && (snakeY[0] == snakeY[i])) {
+            endGame();
+        }
+    }
+}
+
+void checkButtons() {
+    if (PIND.0 == 0) {
+        direction = UP;
+    } else if (PIND.1 == 0) {
+        direction = RIGHT;
+    } else if (PIND.2 == 0) {
+        direction = DOWN;
+    } else if (PIND.3 == 0) {
+        direction = LEFT;
+    }
+}
+
 void main(void) {
-    
-    srand(1);
+    // Pull-up from PD0 to PD3 (BUTTONS)
+    PORTD = 0x0F;
+
+    // Start-up timer 0B in CK/1
+    TCCR0B = 0x01;
+
+    // Configures Matrix and LCD
     ConfiguraMax();
     SetupLCD();
-    
-    PORTD = 0x0F;        // Pull-up de PD0 a PD3
-    snakePos = 19;
-    snakeLen = 1;
+
+    // Create snake position arrays
+    snakeX[0] = 7;
+    snakeY[0] = 7;
+    snakeLength = 1;
+    for (i = 3; i < MAX_LENGTH_SNAKE; i++) {
+        snakeX[i] = -1;
+        snakeY[i] = -1;
+    }
+
+    // Set up game variables
     points = 0;
-    direction = 1;
     speed = 500;
+    direction = RIGHT;
+    gameRunning = 1;
+
+    // Set up points in LCD
     StringLCD("Puntos: ");
     MoveCursor(8,0);
-    while(1) {    
-        // up       
-        CharLCD(points+48);   
-        MoveCursor(8,0);
-        if (PIND.0 == 0) {
-            direction = 0;
-        } else if (PIND.1 == 0) {
-            direction = 1;
-        } else if (PIND.2 == 0) {
-            direction = 2;
-        } else if (PIND.3 == 0) {
-            direction = 3;
+    CharLCD(points+48);   
+    MoveCursor(8,0);
+
+    // Set up random seed
+    srand(TCNT0);
+
+    setPellet();
+
+    while (1) {
+        while (gameRunning) {
+            checkButtons();
+            moveSnake();
+            doEatPellet();
+            doEatSnake();
+
+            clearMax();
+            printBoard();
+            delay_ms(speed);
         }
-        
-        switch (direction) {
-            case 0: {
-                snakePos -= 8;
-                if (snakePos < 0) {  
-                    snakePos += 64;
-                }
-                break;
-            }
-            case 1: {
-                snakePos += 1;
-                if (snakePos%8 == 0) {
-                    snakePos -= 8;
-                }
-                break;
-            }
-            case 2: {     
-                snakePos += 8;
-                if (snakePos > 63) {  
-                    snakePos -= 64;
-                }
-                break;
-            }
-            case 3: {         
-                snakePos -= 1;
-                if (snakePos%8 == 7 || snakePos == -1) {
-                    snakePos += 8;
-                }
-                break;
-            }
-        }
-        
-        pelletCatched = doCatchPellet();
-        if(pelletCatched) {
-            setPellet();
-            pelletCatched = 0;           
-        }
-        MandaMax7219(dots[snakePos]);  
-        MandaMax7219(dots[pelletPos]);
-        
-        delay_ms(speed);
-        clearMax();
+        EraseLCD();
+        StringLCD("Esto ya paro");
     }
 }
